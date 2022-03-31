ff61f99642bb8e777f36ffbd41b520b7b10f6d25
David Palacios <dvalpx@gmail.com>
2022-03-30 22:54:16 -0400

    Changes

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask, render_template, session, redirect, url_for, request, session, flash
-# from functions import db
+from flask import Flask, render_template, session, redirect, url_for, request, session, flash, jsonify
+from functions import db
 
 app = Flask(__name__)
-
 app.secret_key = 'h@1nc34/1_&-jhax32Zesh'
 
 @app.route("/")
 def index():
     if 'login' in session:
-        return render_template("index.html")
+        return render_template("index.html", page_title='Add Plane')
+    return redirect(url_for('login'))
+
+@app.route("/planes", methods=['GET'])
+def planes():
+    if 'login' in session:
+        planes = db.get_planes()
+        return render_template("planes.html", page_title='View Planes', planes=planes)
+    return redirect(url_for('login'))
+
+@app.route("/planes/<int:plane_id>", methods=['GET'])
+def show_plane(plane_id):
+    if 'login' in session:
+        plane = db.get_plane(plane_id)
+        return render_template("plane.html", page_title='Plane Details', plane=plane)
+    return redirect(url_for('login'))
+
+@app.route("/save-plane", methods=['POST'])
+def save_plane():
+    if 'login' in session:
+        plane = db.save_plane(request.form)
+        if plane:
+            flash('Plane saved')
+            return redirect(url_for('index'))
+        return 'error' 
     return redirect(url_for('login'))
 
 @app.route("/login", methods=('GET', 'POST'))
